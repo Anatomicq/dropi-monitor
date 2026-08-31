b5b1383d4a93fd7328c8fd82bede30eb052dbba8
Anatomicq <importacionesyproduccionessas@gmail.com>
2026-08-31 10:04:27 -0500

    limpieza: quitar test/workflow de depuracion de kits

--- test-kits.js
+++
@@ -1,7 +0,0 @@
-const { reportarKits } = require('./reporte-kits');
-(async () => {
-  await reportarKits(
-    { STORE: process.env.SHOPIFY_STORE, CID: process.env.SHOPIFY_CLIENT_ID, CS: process.env.SHOPIFY_CLIENT_SECRET },
-    process.env.SHEETS_WEBAPP_URL, process.env.SHEETS_SECRET
-  );
-})();
